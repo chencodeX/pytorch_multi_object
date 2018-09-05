889c4f293d312309b932a7f301b9262e673b45b7
zihao.chen_tp <zihao.chen@moji.com>
2018-09-05 20:10:03 +0800

    debug

--- f2_2.py
+++ f2_2.py
@@ -64,8 +64,8 @@ class F2_2(nn.Module):
 
     def forward(self, m0):
         T = 5e-4 * m0
-        xmm = np.round(self.xm / self.dlam + 64.5)
-        ymm = np.round(self.ym / self.dlam + 64.5)
+        xmm = np.round(self.xm / self.dlam + 64.5).astype(np.int)
+        ymm = np.round(self.ym / self.dlam + 64.5).astype(np.int)
         print(xmm)
         print(type(xmm))
         print(self.phy[xmm[0]][:,ymm[0]])
