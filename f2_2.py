76b2b75c1907bebc9fde687ba48242285c09dddb
zihao.chen_tp <zihao.chen@moji.com>
2018-09-05 20:48:24 +0800

    debug

--- f2_2.py
+++ f2_2.py
@@ -75,7 +75,7 @@ class F2_2(nn.Module):
         phyxy = self.phy[xmm][:,ymm] + (self.km - self.kh) * T[xmm][:,ymm]
         # print (1j * phyxy)
         p0 = self.ampaxy0 * np.exp(1j * phyxy.data.numpy())
-
+        print p0.size
         self.p01[self.xn:self.xn * 2, self.yn:self.yn * 2] = p0
         pk01 = torch.fft(self.p01, 2)
         pk01 = np.fft.ifftshift(pk01)
